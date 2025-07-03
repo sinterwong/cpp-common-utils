e82390c5ab6ba03cb8a235d34687bce9df43ef3d
SinterWong <sintercver@gmail.com>
2025-07-03 14:31:26 +0800

    feat: add execution exception

--- src/mexception.hpp
+++ src/mexception.hpp
@@ -8,17 +8,16 @@ namespace common_utils::exception {
 #include <type_traits> // Required for std::is_same_v
 
 // Helper type trait to check if a type T is one of Types...
-template <typename T, typename... Types>
-struct is_one_of;
+template <typename T, typename... Types> struct is_one_of;
 
 template <typename T, typename First, typename... Rest>
 struct is_one_of<T, First, Rest...> {
-    static constexpr bool value = std::is_same_v<T, First> || is_one_of<T, Rest...>::value;
+  static constexpr bool value =
+      std::is_same_v<T, First> || is_one_of<T, Rest...>::value;
 };
 
-template <typename T>
-struct is_one_of<T> {
-    static constexpr bool value = false;
+template <typename T> struct is_one_of<T> {
+  static constexpr bool value = false;
 };
 
 template <typename T, typename... Ts>
@@ -30,10 +29,13 @@ const T &get_or_throw(const std::variant<Ts...> &v) {
     // If T is in Ts... but the variant currently holds a different type.
     // This part of the message might be slightly redundant if T wasn't held,
     // but it's clearer than just "Requested type not found".
-    throw std::runtime_error("Variant does not currently hold the requested type: " + std::string(typeid(T).name()));
+    throw std::runtime_error(
+        "Variant does not currently hold the requested type: " +
+        std::string(typeid(T).name()));
   } else {
     // If T is not a possible type for this variant.
-    throw std::runtime_error("Requested type " + std::string(typeid(T).name()) + " is not a valid alternative for this variant");
+    throw std::runtime_error("Requested type " + std::string(typeid(T).name()) +
+                             " is not a valid alternative for this variant");
   }
 }
 
@@ -67,6 +69,11 @@ public:
       : std::runtime_error("Network error: " + message) {}
 };
 
+class ExecutionException : public std::runtime_error {
+public:
+  explicit ExecutionException(const std::string &message)
+      : std::runtime_error("Execution error: " + message) {}
+};
 } // namespace common_utils::exception
 
 #endif
